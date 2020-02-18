15f4f2224f6c6b958d884061713585e9298e2e54
CAM603 <cameronhawley@comcast.net>
2020-02-18 18:48:33 -0500

    ADD get and post endpoints

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,4 +4,47 @@ const db = require('../data/dbConfig');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db('cars')
+    .then(cars => {
+        res.status(200).json(cars);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'Failed to retrieve cars' })
+    });
+});
+
+router.get('/:id', (req, res) => {
+    db('cars')
+        .where({id: req.params.id})
+        .then(car => {
+            res.status(200).json(car)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Failed to retrieve car' })
+        })
+})
+
+router.post('/', (req, res) => {
+    db('cars')
+        .insert(req.body, 'id')
+        .then(ids => {
+            db('cars')
+                .where({id: ids[0]})
+                .then(car => {
+                    res.status(200).json(car)
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.status(500).json({ error: 'Failed to get car' })
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to add car' })
+        })
+})
+
 module.exports = router;
